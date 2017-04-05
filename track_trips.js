3c35b6af6675daf561c9e373c566db6f8b868525
Sean Miller <miller.sean@byu.edu>
2017-04-04 20:47:35 -0600

    randomjs

---
+++ track_trips.js
@@ -0,0 +1,20 @@
+var args = require("minimist")(process.argv.slice(2));
+ 
+var event_type = args.e || "message";
+var message = args.m || "test";
+ 
+var request = require("request");
+// must be an eci to a pico with the echo_server ruleset or similar installed
+var eci = "cj12uyjrb0000ug7bjaogafn8";
+var eid = "my-event-id";
+var pico_engine = "localhost:8080";
+ 
+var url = "http://"+pico_engine+"/sky/event/"+eci+"/"+eid+"/track_trips/"+event_type;
+request.post(url,{form:{input:message}},function(err,response,body){
+  JSON.parse(body).directives.forEach(function(d){
+    if (d.name === "trip"){
+      console.log(d.options.trip_length);
+      console.log(d.options.mileage);
+    }
+  });
+});
