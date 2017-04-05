1d0bfe2ab3e3db8281a1a82fb303221c2aee9b8f
Sean Miller <miller.sean@byu.edu>
2017-04-04 21:27:00 -0600

    part2 begin

--- track_trips2.js
+++ track_trips2.js
@@ -1,7 +1,7 @@
 var args = require("minimist")(process.argv.slice(2));
  
-var event_type = args.e || "message";
-var message = args.m || "test";
+var event_type = args.e || "car";
+var message = args.m || "";
  
 var request = require("request");
 // must be an eci to a pico with the echo_server ruleset or similar installed
@@ -10,7 +10,7 @@ var eid = "my-event-id";
 var pico_engine = "localhost:8080";
  
 var url = "http://"+pico_engine+"/sky/event/"+eci+"/"+eid+"/track_trips/"+event_type;
-request.post(url,{form:{input:message}},function(err,response,body){
+request.post(url,{form:{input:new_message}},function(err,response,body){
   JSON.parse(body).directives.forEach(function(d){
     if (d.name === "trip"){
       console.log(d.options.trip_length);
